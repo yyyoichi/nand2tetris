f8aa6a26723ffb1e79637815cf66f4be1835265e
yyyoichi <origamiichiro@gmail.com>
2023-01-24 09:27:01 +0900

    add screen fill

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,58 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+@16896
+D=A
+@1
+M=D
+
+(MAIN)
+@SCREEN
+D=A
+@0
+M=D
+
+@24576
+D=M
+@OUT
+D;JEQ
+@FILL
+0;JMP
+
+(FILL)
+@0
+A=M// 点灯させる位置
+M=-1// 点灯
+
+@24576
+D=M
+@MAIN
+D;JEQ
+
+@0
+MD=M+1// かうんた
+@1
+D=M-D
+@MAIN
+D;JLT// 終了
+@FILL
+0;JMP
+
+(OUT)
+@0
+A=M// 消灯させる位置
+M=0// 消灯
+
+@24576
+D=M
+@MAIN
+D;JGT
+
+@0
+MD=M+1// かうんた
+@1
+D=M-D
+@MAIN
+D;JLT// 終了
+@OUT
+0;JMP
